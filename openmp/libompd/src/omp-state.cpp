7845d9d4af99c8770a69e8f3b08a1833e124d5ae
nagajyothi-dev <68853886+nagajyothi-dev@users.noreply.github.com>
2020-12-11 19:08:25 +0530

    Update omp-state.cpp

--- openmp/libompd/src/omp-state.cpp
+++ openmp/libompd/src/omp-state.cpp
@@ -1,3 +1,15 @@
+/*
+ * omp-state.cpp -- OMPD states
+ */
+
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "ompd-private.h"
 #include "omp-debug.h"
 #include <cstring>
