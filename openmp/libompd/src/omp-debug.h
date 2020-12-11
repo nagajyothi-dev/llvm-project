ef470558846d4c3cac2ebbb82375468e8f7e4767
nagajyothi-dev <68853886+nagajyothi-dev@users.noreply.github.com>
2020-12-11 18:47:30 +0530

    Update omp-debug.h

--- openmp/libompd/src/omp-debug.h
+++ openmp/libompd/src/omp-debug.h
@@ -7,6 +7,15 @@
  *     Contact: ilaguna@llnl.gov
  *              protze@llnl.gov
  */
+
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef SRC_OMP_DEBUG_H_
 #define SRC_OMP_DEBUG_H_
 
