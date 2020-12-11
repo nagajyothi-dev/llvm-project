59cb385d83bf381694c3680e16f58ff4d04f9b0c
nagajyothi-dev <68853886+nagajyothi-dev@users.noreply.github.com>
2020-12-11 18:58:26 +0530

    Update ompd-types.h

--- openmp/libompd/src/ompd-types.h
+++ openmp/libompd/src/ompd-types.h
@@ -1,6 +1,15 @@
 /*
 * @@name:   ompd_types.h
 */
+
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef __OPMD_TYPES_H
 #define __OPMD_TYPES_H
 
