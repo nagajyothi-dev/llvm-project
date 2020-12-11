4f5f0906ba39ebe5c337c39169d497e68ebeca3a
nagajyothi-dev <68853886+nagajyothi-dev@users.noreply.github.com>
2020-12-11 17:23:56 +0530

    Add license text to OMPD files.

--- openmp/runtime/src/ompd-specific.cpp
+++ openmp/runtime/src/ompd-specific.cpp
@@ -1,3 +1,15 @@
+/*
+ * ompd_specific.cpp -- OpenMP debug support
+ */
+
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "ompd-specific.h"
 
 #ifdef OMPD_SUPPORT
