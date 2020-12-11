37489e05d6e92289ce104512d960f00a721d2e80
nagajyothi-dev <68853886+nagajyothi-dev@users.noreply.github.com>
2020-12-11 19:05:27 +0530

    Update omp-debug.cpp

--- openmp/libompd/src/omp-debug.cpp
+++ openmp/libompd/src/omp-debug.cpp
@@ -11,6 +11,13 @@
  * This implements an OMPD DLL for the LLVM OpenMP runtime library.
  */
 
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 #define NDEBUG 1
 
 #include "omp-debug.h"
