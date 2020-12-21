b27acfc1861ba48ef31aeb61eac138f92f53b5ba
nagajyothi-dev <68853886+nagajyothi-dev@users.noreply.github.com>
2020-12-21 15:39:02 +0530

    Update ompd-specific.h
    Add LLVM license text.

--- openmp/runtime/src/ompd-specific.h
+++ openmp/runtime/src/ompd-specific.h
@@ -1,3 +1,15 @@
+/*
+ * ompd-specific.h -- OpenMP debug support
+ */
+
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "kmp.h"
 #include "omp-tools.h"
 #include <stdint.h>
