a8381e25cffcf71e8eecadcf915dd90791a1b97a
nagajyothi-dev <68853886+nagajyothi-dev@users.noreply.github.com>
2020-12-21 15:48:45 +0530

    Update Debug.cpp
    Add LLVM license text.

--- openmp/libompd/src/Debug.cpp
+++ openmp/libompd/src/Debug.cpp
@@ -1,3 +1,15 @@
+/*
+ * Debug.cpp -- OMP debug
+ */
+
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "Debug.h"
 
 std::ostream &GdbColor::operator<<(std::ostream &os, GdbColor::Code code) {
